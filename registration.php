89b61ff3edd03c9f6bf5204c96dc5a4ba42cf975
Menard Gardose <mjgardose@gmail.com>
2018-01-18 15:25:38 +0800

    added new file

--- registration.php
+++ registration.php
@@ -1,3 +1,21 @@
+<?php?
+	$mysqli = new sqli("localhost", "root", 
+	"", "student")
+
+    $errnum=mysqli_connect_errno();
+    if ($errnum)
+    {
+    	$errmsg=mysqli_connect_errno();
+
+    	print "Connect failed. error number= $errnum"
+    	"<br /> error message = $errmsg";
+    	exit();
+    }
+
+?>
+
+
+
 <html>
 
 <head>
